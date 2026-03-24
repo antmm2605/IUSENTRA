0a805cb42df14f469e43633d023b895b02554618
antmm2605 <82462746+antmm2605@users.noreply.github.com>
2026-03-24 20:33:24 +0100

    Add files via upload

--- pct/__init__.py
+++ pct/__init__.py
@@ -3,9 +3,5 @@ PCT - Processo Civile Telematico
 Sistema per l'invio telematico negli studi legali italiani.
 """
 
-<<<<<<< HEAD
-__version__ = "2.26.7"
-=======
 __version__ = "2.30.3"
->>>>>>> 62e744dc5064f0e995063551f5be5b7c3c5acf0f
 __author__ = "Studio Legale"

4284c6d3516c7a515c492729ebe0f61ac580ff3a
antmm2605 <82462746+antmm2605@users.noreply.github.com>
2026-03-27 18:17:08 +0100

    Update vercel_app.py

--- vercel_app.py
+++ vercel_app.py
@@ -3,6 +3,7 @@ from web.app import create_app
 
 # Forziamo una configurazione compatibile con Vercel
 os.environ['PCT_STUDIO_CONFIG'] = '/tmp/studio.json' 
+os.environ['DATABASE_URL'] = 'sqlite:////tmp/database.db'
 
 app = create_app()
 
